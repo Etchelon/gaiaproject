00579f35c520c9a57d3404270635e3cd9fdd4a1e
Andrea Bertoldo <andrea.bertoldo.btf@outlook.com>
2021-03-30 14:10:56 +0200

    cleanup: remove unused files; remove game notes and notifications at game end (#22)

--- be.bat
+++
@@ -1,1 +0,0 @@
-cd ./Backend/Endpoint/ & $Env:ASPNETCORE_ENVIRONMENT="Development" & dotnet run --no-build --urls="https://localhost:5001;http://localhost:5000"
--- beb.bat
+++
@@ -1,1 +0,0 @@
-cd ./Backend/Endpoint/ & $Env:ASPNETCORE_ENVIRONMENT="Development" & dotnet run --urls="https://localhost:5001;http://localhost:5000"
