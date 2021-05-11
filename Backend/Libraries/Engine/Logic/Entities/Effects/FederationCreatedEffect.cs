bafb69730f358aac318c11aa88842d918bf0211f
Andrea Bertoldo <andrea.bertoldo.btf@outlook.com>
2021-05-12 00:59:36 +0200

    fix: persist to DB the federation marker. Closes #67

--- Backend/Libraries/Engine/Logic/Entities/Effects/FederationCreatedEffect.cs
+++ Backend/Libraries/Engine/Logic/Entities/Effects/FederationCreatedEffect.cs
@@ -7,30 +7,34 @@ using GaiaProject.Engine.Model.Players;
 
 namespace GaiaProject.Engine.Logic.Entities.Effects
 {
-    public class FederationCreatedEffect : Effect
-    {
-        public List<Hex> FederatedHexes { get; }
+	public class FederationCreatedEffect : Effect
+	{
+		public List<Hex> FederatedHexes { get; }
 
-        public FederationCreatedEffect(List<Hex> federatedHexes)
-        {
-            FederatedHexes = federatedHexes;
-        }
+		public FederationCreatedEffect(List<Hex> federatedHexes)
+		{
+			FederatedHexes = federatedHexes;
+		}
 
-        public override void ApplyTo(GaiaProjectGame game)
-        {
-            var player = game.GetPlayer(PlayerId);
-            var counter = player.State.Federations.Count() + 1;
-            var newFederation = Federation.FromBuildings(PlayerId, counter, FederatedHexes);
-            player.State.Federations.Add(newFederation);
+		public override void ApplyTo(GaiaProjectGame game)
+		{
+			var player = game.GetPlayer(PlayerId);
+			var counter = player.State.Federations.Count() + 1;
+			var newFederation = Federation.FromBuildings(PlayerId, counter, FederatedHexes);
+			player.State.Federations.Add(newFederation);
 
-            // If the federation has no satellites, mark one of the buildings with the Federation Marker
-            var buildings = FederatedHexes.SelectMany(h => h.Buildings).ToList();
-            if (buildings.Any(b => b.Type == BuildingType.Satellite || b.Type == BuildingType.IvitsSpaceStation))
-            {
-                return;
-            }
-            var buildingToMark = buildings.OrderByDescending(b => b.PowerValueInFederation).First();
-            buildingToMark.ShowFederationMarker = true;
-        }
-    }
+			// If the federation has no satellites, mark one of the buildings with the Federation Marker
+			var buildings = FederatedHexes
+				.SelectMany(h => h.Buildings)
+				.Where(b => b.PlayerId == PlayerId)
+				.ToList();
+			if (buildings.Any(b => b.Type == BuildingType.Satellite || b.Type == BuildingType.IvitsSpaceStation))
+			{
+				return;
+			}
+			var buildingToMark = buildings.OrderByDescending(b => b.PowerValueInFederation).First();
+			var hex = game.BoardState.Map.Hexes.Single(h => h.Id == buildingToMark.HexId);
+			hex.Buildings.Single(b => b.Id == buildingToMark.Id).ShowFederationMarker = true;
+		}
+	}
 }
