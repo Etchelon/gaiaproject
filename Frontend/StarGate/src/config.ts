7dc6ee352e48d91e04574c52b6093fbe98d0c20a
Andrea Bertoldo <andrea.bertoldo.btf@outlook.com>
2022-03-02 20:55:54 +0100

    chore: remove the create-react-app Stargate folder

--- Frontend/StarGate/src/config.ts
+++
@@ -1,1 +0,0 @@
-export const BASE_URL = process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_BASE_URL! : "http://<hostname>:5000";
--- Frontend/StarGate/src/frame/store/types.ts
+++
@@ -1,9 +0,0 @@
-import { NotificationDto } from "../../dto/interfaces";
-import { LoadingStatus } from "../../games/store/types";
-
-export interface ActiveUserState {
-	drawerState: "open" | "close";
-	unreadNotificationsCount: number;
-	notifications: NotificationDto[];
-	notificationsState: LoadingStatus;
-}
